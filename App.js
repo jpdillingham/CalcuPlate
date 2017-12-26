3bcc22d667f610f5192ef107700a2ac472b0c851
JP Dillingham <jpdillingham@gmail.com>
2017-12-26 15:17:09 -0600

    Added Plate component

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Modal, Button } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Modal, Button, TextInput } from 'react-native';
+
+import Plate from './Plate'
 
 export default class App extends React.Component {
   state = {
-    isModalVisible: false
+    isModalVisible: false,
+    input: ''
   }
 
   _showModal = () => this.setState({ isModalVisible: true })
 
   _hideModal = () => this.setState({ isModalVisible: false })
+  handleInput = (input) => { 
+    this.setState({ input: input })
+  }
 
   render () {
     return (
       <View style={styles.container}>
+        <Plate weight="1.25"/>
+        <Plate weight="2.5"/>
+        <Plate weight="5"/>
+        <Plate weight="10"/>
+        <Plate weight="25"/>
+        <Plate weight="35"/>
+        <Plate weight="45"/>
+        <Plate weight="55"/>
+
+        <TextInput style = {styles.input}
+               keyboardType = "numeric"
+               underlineColorAndroid = "transparent"
+               placeholder = "Desired Weight"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleInput}/>
         <TouchableOpacity onPress={this._showModal}>
           <Text>Show Modal</Text>
         </TouchableOpacity>
@@ -20,14 +42,13 @@ export default class App extends React.Component {
           visible={this.state.isModalVisible} 
           onRequestClose={this._hideModal} 
           style={styles.modal}
-          transparent={true}
           animationType='slide'
         >
           <View style={styles.modal}>
             <Text>Hello</Text>
             <Button
               onPress={this._hideModal}
-              title="Learn More"
+              title="Close Modal"
               color="#841584"
               accessibilityLabel="Learn more about this purple button"
             />
@@ -40,10 +61,13 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    top: 40,
     flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    width: 300,
   },
   modal: {
     backgroundColor: '#808080',
---
+++ Plate.js
@@ -0,0 +1,28 @@
+import React, { Component} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Modal, Button, TextInput } from 'react-native';
+
+class Plate extends Component {
+    sizes = [ '1.25', '2.5', '5', '10', '25', '35', '45', '55' ]
+    widths = [ 50, 100, 150, 200, 250, 300, 350, 400 ]
+
+    noop = () => { }
+
+    getWidth = (weight) => { 
+        return this.widths[this.sizes.indexOf(weight)]
+    }
+
+    render () {
+        return (
+            <View style={{width: this.getWidth(this.props.weight), padding: 1 }}>
+                <Button
+                    onPress={this.noop}
+                    title={this.props.weight}
+                    color="#505050"
+                    accessibilityLabel={this.props.weight}
+                />
+            </View>
+        )
+    }
+}
+
+export default Plate
