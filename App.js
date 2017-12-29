d33662bfb0e0d6454431147bb67a853142f1d533
JP Dillingham <jpdillingham@gmail.com>
2017-12-29 10:11:33 -0600

    Updated line spacing

--- App.js
+++ App.js
@@ -7,24 +7,24 @@ import Input from './components/Input'
 import Settings from './components/Settings'
 
 export default class App extends React.Component {
-	state = {
-		bar: 45,
-		bars: [ 40, 45, 50, 55 ],
-		plates: [
+    state = {
+        bar: 45,
+        bars: [ 40, 45, 50, 55 ],
+        plates: [
             { key: 1.25, count: 1 },
-			{ key: 2.5, count: 1 },
-			{ key: 5, count: 2 },
-			{ key: 10, count: 2 },
-			{ key: 25, count: 1 },
-			{ key: 35, count: 1 },
-			{ key: 45, count: 1 },
-			{ key: 55, count: 0 },
-		],
+            { key: 2.5, count: 1 },
+            { key: 5, count: 2 },
+            { key: 10, count: 2 },
+            { key: 25, count: 1 },
+            { key: 35, count: 1 },
+            { key: 45, count: 1 },
+            { key: 55, count: 0 },
+        ],
         desiredWeight: 45,
-	}
+    }
 
-	handleInput = (input) => { 
-		this.setState({ desiredWeight: input })
+    handleInput = (input) => { 
+        this.setState({ desiredWeight: input })
     }
     
     handleBarUpdate = (weight) => {
@@ -32,40 +32,40 @@ export default class App extends React.Component {
     }
 
     handlePlateUpdate = (key, count) => {
-		let newPlates = JSON.parse(JSON.stringify(this.state.plates));
+        let newPlates = JSON.parse(JSON.stringify(this.state.plates));
 
-		let foundPlate = newPlates.find(p => p.key == key);
-        
+        let foundPlate = newPlates.find(p => p.key == key);
+
         if (foundPlate) {
-			let foundIndex = newPlates.indexOf(foundPlate);
-			newPlates[foundIndex].count = count;
+            let foundIndex = newPlates.indexOf(foundPlate);
+            newPlates[foundIndex].count = count;
 
-			this.setState({ plates: newPlates })
-		}
+            this.setState({ plates: newPlates })
+        }
     }
 
-	render () {
-		return (
-			<View style={styles.container}>
-				<View style={styles.contentContainer}>
-					<Title/>
-					<PlateDisplay 
-						bar={this.state.bar}
-						plates={this.state.plates} 
-						weight={this.state.desiredWeight}
-					/>
-					<Input handleInput={this.handleInput}/>
-				</View>
+    render () {
+        return (
+            <View style={styles.container}>
+                <View style={styles.contentContainer}>
+                    <Title/>
+                    <PlateDisplay 
+                        bar={this.state.bar}
+                        plates={this.state.plates} 
+                        weight={this.state.desiredWeight}
+                    />
+                    <Input handleInput={this.handleInput}/>
+                </View>
                 <Settings 
                     plates={this.state.plates} 
-					handlePlateUpdate={this.handlePlateUpdate} 
-					bars={this.state.bars}
+                    handlePlateUpdate={this.handlePlateUpdate} 
+                    bars={this.state.bars}
                     bar={this.state.bar} 
                     handleBarUpdate={this.handleBarUpdate}
                 />
-			</View>
-		)
-	}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
